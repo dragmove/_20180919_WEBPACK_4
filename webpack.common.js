1e057cf629747d5e17c138e693cd644e03955288
dragmove <dragmove@naver.com>
2018-10-24 02:11:22 +0900

    feat: Distribute webpack.config.js to 3 files

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,57 @@
 const path = require('path');
+
+// https://webpack.js.org/guides/production/
+module.exports = {
+  // devtool:
+
+  // devServer:
+
+  entry: {
+    client: './public/client.js'
+  },
+
+  externals: {}, // https://webpack.js.org/configuration/externals/
+
+  // mode:
+
+  module: {
+    // https://webpack.js.org/concepts/loaders/
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }, { loader: 'eslint-loader' }]
+      }
+    ]
+  },
+
+  // plugins:
+
+  // optimization:
+
+  output: {
+    // publicPath: '',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  target: 'web' // https://webpack.js.org/configuration/target/
+};
+
+// TODO: Read next articles
+// https://webpack.js.org/guides/code-splitting/
+
+/*
+// webpack.config.js
+
+const path = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env, argv) => {
   // https://webpack.js.org/configuration/dev-server/
-  let devServer = {
+  const devServer = {
     compress: true,
     contentBase: __dirname, // path.join(__dirname, 'build'),
     // host: '0.0.0.0',
@@ -14,18 +61,22 @@ module.exports = (env, argv) => {
     publicPath: '/' // https://webpack.js.org/configuration/dev-server/#devserver-publicpath-
   };
 
+  const target = 'web';
+
   let devtool = '';
   let externals = {};
   let plugins = [];
+  let mode = '';
   let minimize = false;
   let minimizer = [];
   let usedExports = false;
-  let target = 'web';
 
   switch (argv.mode) {
     // https://webpack.js.org/concepts/mode/#mode-development
     case 'development':
       devtool = 'eval-source-map'; // https://webpack.js.org/configuration/devtool/
+
+      mode = 'development';
 
       minimize = false;
 
@@ -54,6 +105,9 @@ module.exports = (env, argv) => {
       usedExports = false;
 
       plugins = [
+        // https://webpack.js.org/guides/output-management/#cleaning-up-the-dist-folder
+        // new CleanWebpackPlugin(['build']),
+
         // https://webpack.js.org/plugins/define-plugin/
         new webpack.DefinePlugin({
           // global constants in development mode
@@ -65,6 +119,8 @@ module.exports = (env, argv) => {
     // https://webpack.js.org/concepts/mode/#mode-production
     case 'production':
       devtool = 'source-map';
+
+      mode = 'production';
 
       minimize = true;
 
@@ -92,6 +148,9 @@ module.exports = (env, argv) => {
       usedExports = true;
 
       plugins = [
+        // https://webpack.js.org/guides/output-management/#cleaning-up-the-dist-folder
+        new CleanWebpackPlugin(['build']),
+
         // https://webpack.js.org/plugins/define-plugin/
         new webpack.DefinePlugin({
           // global constants in development mode
@@ -111,7 +170,7 @@ module.exports = (env, argv) => {
 
     externals: externals, // https://webpack.js.org/configuration/externals/
 
-    mode: 'development', // 'development' or 'production'. $webpack --mode=production
+    mode: mode, // 'development' or 'production'. $webpack --mode=production
 
     module: {
       // https://webpack.js.org/concepts/loaders/
@@ -144,3 +203,4 @@ module.exports = (env, argv) => {
     // https://webpack.js.org/guides/asset-management/
   };
 };
+*/
