b025da0df65e2fdedfb1621454f839777622e430
김현석 <dragmove@naver.com>
2018-12-12 15:12:20 +0900

    feat: Arrange client.js

--- public/client.js
+++ public/client.js
@@ -2,63 +2,53 @@ import _ from 'lodash';
 import { isDefined } from './utils';
 import { printHot } from './hot';
 
-if (isDefined('hello client.js')) console.log('This is client.js');
+(() => {
+  'use strict';
 
-/*
- * @babel/plugin-proposal-optional-chaining
- */
-const obj = {
-  foo: {
-    bar() {
-      return 42;
-    }
+  if (isDefined('hello client.js')) console.log('This is client.js');
+
+  /*
+   * @babel/plugin-proposal-optional-chaining
+   */
+  const obj = { foo: () => 99 };
+  console.log('obj?.foo() :', obj?.foo());
+
+  /*
+   * dynamic imports (https://webpack.js.org/guides/code-splitting/#dynamic-imports)
+   */
+  document.body.appendChild(component());
+
+  function component() {
+    let element = document.createElement('div');
+    let button = document.createElement('button');
+    let br = document.createElement('br');
+
+    button.innerHTML = 'Click me and look at the console!';
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    // Note that because a network request is involved, some indication
+    // of loading would need to be shown in a production-level site/app.
+    button.onclick = evt =>
+      import(/* webpackChunkName: "print" */ './print').then(module => {
+        const print = module.default;
+
+        print();
+      });
+
+    return element;
   }
-};
 
-// FIXME: obj.foo.bar()
-if (obj && obj.foo && obj.foo.bar) {
-  // console.log('obj.foo.bar() :', obj.foo.bar());
-  // console.log('obj.hello.bar() :', obj.hello.bar());
-}
-// TO:
-// console.log('obj?.foo?.bar() :', obj?.foo?.bar());
-// console.log('obj?.hello?.bar() :', obj?.hello?.bar());
+  /*
+   * https://webpack.js.org/guides/hot-module-replacement/
+   */
+  if (module.hot) {
+    module.hot.accept('./hot.js', function() {
+      console.log('Accepting the updated printHot module');
 
-/*
- * dynamic imports (https://webpack.js.org/guides/code-splitting/#dynamic-imports)
- */
-document.body.appendChild(component());
-
-function component() {
-  let element = document.createElement('div');
-  let button = document.createElement('button');
-  let br = document.createElement('br');
-
-  button.innerHTML = 'Click me and look at the console!';
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(br);
-  element.appendChild(button);
-
-  // Note that because a network request is involved, some indication
-  // of loading would need to be shown in a production-level site/app.
-  button.onclick = evt =>
-    import(/* webpackChunkName: "print" */ './print').then(module => {
-      const print = module.default;
-
-      print();
+      printHot();
     });
-
-  return element;
-}
-
-/*
- * https://webpack.js.org/guides/hot-module-replacement/
- */
-if (module.hot) {
-  module.hot.accept('./hot.js', function() {
-    console.log('Accepting the updated printHot module');
-
-    printHot();
-  });
-}
+  }
+})();
